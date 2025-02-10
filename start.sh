ccd1c758eafbab13610107840a47ad5ff106fd3a
smallyunet <dev.wangyu@gmail.com>
2025-02-11 00:24:55 +0800

    init
    Signed-off-by: smallyunet <dev.wangyu@gmail.com>

---
+++ start.sh
@@ -0,0 +1,12 @@
+
+docker run \
+  -d \
+  --restart unless-stopped \
+  --name=dora-explorer \
+  -v $(pwd):/config \
+  -v $(pwd)/network-config:/network-config \
+  -p 9777:8080 \
+  -it ethpandaops/dora:latest \
+  -config=/config/config.yaml
+
+
